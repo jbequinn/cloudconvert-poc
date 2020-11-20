ae2ecb07e135e130a20933290225b8bfd085463c
jbequinn <jbequinn@gmail.com>
2020-11-20 17:44:06 +0100

    [cloudconvert-poc] create skeleton for unit tests class

---
+++ src/test/java/com/github/jbequinn/poc/cloudconvert/CloudServiceTest.java
@@ -0,0 +1,11 @@
+package com.github.jbequinn.poc.cloudconvert;
+
+import org.junit.jupiter.api.Test;
+
+public class CloudServiceTest {
+	@Test
+	void formParametersAreParsedAndUsed() {
+		// wiremock a response to import/upload with the json form element
+		// then verify that the other POST arrives with those form parameters
+	}
+}
