92d8950213152b949e9c8fc325b5ef564073b312
jbequinn <jbequinn@gmail.com>
2020-11-22 13:49:26 +0100

    [cloudconvert-poc] add system properties for the input and output files
    but having a value as fallback

--- src/main/java/com/github/jbequinn/poc/cloudconvert/AppProperties.java
+++ src/main/java/com/github/jbequinn/poc/cloudconvert/AppProperties.java
@@ -5,15 +5,23 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import static java.util.Objects.requireNonNullElse;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class AppProperties {
 	@Builder.Default
-	private String inputFilePath = "/tmp/test-file.docx";
+	private String inputFilePath = requireNonNullElse(
+			System.getProperty("CLOUDCONVERT_INPUT_FILE"),
+			"/tmp/test-file.docx"
+	);
 	@Builder.Default
-	private String outputFilePath = "/tmp/test-file.pdf";
+	private String outputFilePath = requireNonNullElse(
+			System.getProperty("CLOUDCONVERT_OUTPUT_FILE"),
+			"/tmp/test-file.pdf"
+	);
 	@Builder.Default
 	private String jobsUrl = "https://api.cloudconvert.com/v2/jobs";
 	private String accessToken;
